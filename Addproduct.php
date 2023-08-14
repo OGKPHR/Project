8470ea832551076f6806902c643a7215c9c4bc3c
Pacharapol Wannachai <120402752+OGKPHR@users.noreply.github.com>
2023-08-14 13:45:52 +0700

    Add files via upload

--- Addproduct.php
+++ Addproduct.php
@@ -33,11 +33,39 @@
                 echo "Error deleting products: " . mysqli_error($conn);
             }
         }
+           
     }
 
     // Fetch products from the database
     $query = "SELECT * FROM product";
     $result = mysqli_query($conn, $query);
+
+    // Count total users in the database
+    $countQuery = "SELECT COUNT(id) AS total_users FROM user";
+    $countResult = mysqli_query($conn, $countQuery);
+    $totalUsers = mysqli_fetch_assoc($countResult)['total_users'];
+
+    // Search for users by name or ID
+    if (isset($_POST['search'])) {
+        $searchTerm = $_POST['search_term'];
+        $searchQuery = "SELECT * FROM user WHERE id = '$searchTerm' OR username = '$searchTerm' OR CONCAT(firstname, ' ', lastname) LIKE '%$searchTerm%'";
+        $searchResult = mysqli_query($conn, $searchQuery);
+    }
+    // Handle user deletion
+if (isset($_POST['delete_users'])) {
+    if (!empty($_POST['selected_users'])) {
+        $selectedUsers = $_POST['selected_users'];
+        $ids = implode(',', $selectedUsers);
+        $deleteQuery = "DELETE FROM user WHERE id IN ($ids)";
+        $deleteResult = mysqli_query($conn, $deleteQuery);
+
+        if ($deleteResult) {
+            echo "Selected users deleted successfully!";
+        } else {
+            echo "Error deleting users: " . mysqli_error($conn);
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -46,7 +74,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add Product Page</title>
+    <title>Product Management</title>
     <link rel="icon" href="unnamed.png">
     <link rel="stylesheet" href="APD.css">
 </head>
@@ -55,9 +83,9 @@
         <h3>Admin: <?php echo $_SESSION['fname']; ?></h3>
         <a href="logout.php">ออกจากระบบ</a>
     </div>
-   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <h2>เพิ่มสินค้า</h2>
-    <label for="phonenumber">หมายเลขเบอร์โทร</label>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <h2>เพิ่มสินค้า</h2>
+        <label for="phonenumber">หมายเลขเบอร์โทร</label>
     <input type="text" name="phonenumber" placeholder="กรอกเบอร์โทร" required>
     <br>
     <label for="Provider">ผู้ให้บริการ</label>
@@ -70,8 +98,8 @@
     <label for="price">ราคา</label>
     <input type="text" name="price" placeholder="กรอกราคา" required>
     <br>
-    <input type="submit" name="submit" value="ยืนยัน">
-</form>
+        <input type="submit" name="submit" value="ยืนยัน">
+    </form>
 
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <h2>ลบสินค้า</h2>
@@ -97,5 +125,53 @@
         </table>
         <button type="submit" name="delete">ลบสินค้าที่เลือก</button>
     </form>
+
+    
+    <!-- Search users by name or ID -->
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <p><h2>ค้นหาผู้ใช้</h2><h2><div class="usamount">จำนวนผู้ใช้ในระบบ: <?php echo $totalUsers; ?></div></h2></p>
+
+        <label for="search_term">ค้นหาด้วยชื่อหรือรหัสผู้ใช้</label>
+        <input type="text" name="search_term" placeholder="กรอกชื่อหรือรหัสผู้ใช้" required>
+        <button type="submit" name="search">ค้นหา</button>
+    </form>
+
+    <!-- Display searched users -->
+    <?php if (isset($_POST['search'])) : ?>
+       
+        <?php if (mysqli_num_rows($searchResult) > 0) : ?>
+         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+         <h2>ผลการค้นหาผู้ใช้</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>เลือก</th>
+                        <th>ID</th>
+                        <th>ชื่อ</th>
+                        <th>นามสกุล</th>
+                        <th>สถานะ</th>
+                        <!-- Add more columns if needed... -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = mysqli_fetch_assoc($searchResult)) : ?>
+                        <tr>
+                            <td><input type="checkbox" name="selected_users[]" value="<?php echo $row['id']; ?>"></td>
+                            <td><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['firstname']; ?></td>
+                            <td><?php echo $row['lastname']; ?></td>
+                            <td><?php echo ($row['userlevel'] == 'a') ? 'ADMIN' : (($row['userlevel'] == 'm') ? 'USER' : ''); ?></td>
+
+                            <!-- Add more columns if needed... -->
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+            <button type="submit" name="delete_users">ลบผู้ใช้ที่เลือก</button>
+        </form>
+        <?php else : ?>
+           <div class="red">ไม่พบผู้ใช้ที่ค้นหา</div> 
+        <?php endif; ?>
+    <?php endif; ?>
 </body>
 </html>
--- Shop.js
+++ Shop.js
@@ -1,5 +1,5 @@
 // JavaScript code for the cart system
-const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+const cartItems = [];
 const cartTotal = document.getElementById('cart-total');
 const cartItemsList = document.getElementById('cart-items');
 const clearCartBtn = document.getElementById('clear-cart-btn');
@@ -8,14 +8,7 @@ const clearCartBtn = document.getElementById('clear-cart-btn');
 function isProductInCart(productName) {
     return cartItems.some(item => item.productName === productName);
 }
-
-// Function to add an item to the cart
-function addToCart(productName, price) {
-    if (!isProductInCart(productName)) {
-        cartItems.push({ productName, price });
-        updateCart();
-    }
-}
+    
 
 // Function to update the cart content and total
 function updateCart() {
@@ -28,7 +21,7 @@ function updateCart() {
 
         // Add a delete button for each cart item
         const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'ลบ'; //(ลบ)
+        deleteBtn.textContent = 'ลบ'; // You can customize the label
         deleteBtn.addEventListener('click', () => {
             removeFromCart(index);
         });
@@ -40,11 +33,14 @@ function updateCart() {
     });
 
     cartTotal.textContent = `฿${total.toFixed(2)}`;
-    saveCartToLocalStorage();
-    updateAddToCartButtons();
+}
 
-    // Update "Add to Cart" buttons based on the cart content
-    updateAddToCartButtons();
+// Function to add an item to the cart
+function addToCart(productName, price) {
+    if (!isProductInCart(productName)) {
+        cartItems.push({ productName, price });
+        updateCart();
+    }
 }
 
 // Function to remove an item from the cart
@@ -60,25 +56,17 @@ function clearCart() {
 }
 
 // Event listener for the "Add to Cart" buttons
-function updateAddToCartButtons() {
-    const addToCartButtons = document.querySelectorAll('.product button');
-    addToCartButtons.forEach(button => {
+const addToCartButton = document.querySelectorAll('.product button');
+        addToCartButton.forEach(button => {
+    button.addEventListener('click', () => {
         const productDiv = button.closest('.product');
         const productName = productDiv.querySelector('h2').textContent;
-        button.disabled = isProductInCart(productName);
-        button.textContent = isProductInCart(productName) ? 'อยู่ในตะกร้า' : 'ใส่ในตะกร้า'; // (ใส่ในตะกร้า)
+        const price = parseFloat(productDiv.querySelector('.price').textContent.slice(1));
+        addToCart(productName, price);
     });
-}
+});
 
 // Event listener for the "Clear Cart" button
 clearCartBtn.addEventListener('click', () => {
     clearCart();
 });
-
-// Function to save the cart to Local Storage
-function saveCartToLocalStorage() {
-    localStorage.setItem('cart', JSON.stringify(cartItems));
-}
-
-// Initial update of the cart content
-updateCart();
